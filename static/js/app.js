085c1e8b010e6459ac3bffe1574adb05fb295b32
Chloe Veras <chloe@chloes-air.lan>
2020-12-22 15:48:04 -0500

    Populated dropdown menu with sample names

--- static/js/app.js
+++ static/js/app.js
@@ -1,79 +1,73 @@
-test = {
-    "names":["940", "941", "943", "944", "945", "946", "947", "948"],
-    "metadata":[{"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0}, {"id": 941, "ethnicity": "Caucasian/Midleastern", "gender": "F", "age": 34.0, "location": "Chicago/IL", "bbtype": "I", "wfreq": 1.0}]
-}
-
-
 // Define a function that will create metadata for given sample
-function buildMetadata(sample) {
+function buildMetadata(selection) {
 
     // Read the json data
-    d3.json("../samples.json").then((data) => {
-        
-        //console.log(data);
+    d3.json("../samples.json").then((sampleData) => {
 
-        //console.log("next")
-        
+        console.log(sampleData);
+
+        console.log("showing parsed data:");
+
         // Parse and filter the data to get the sample's metadata
-        //var parsedData = JSON.parse(sample); 
-        var parsedData = data; 
-        var metadata = parsedData.metadata; 
-        //console.log(metadata); 
+        var parsedData = sampleData.metadata;
+        console.log(parsedData);
+
+        var sample = parsedData.filter(item => item.id == selection);
+        console.log("showing sample[0]:");
+        console.log(sample[0]);
 
         // Specify the location of the metadata and update it
-        var addingData = d3.select("#sample-metadata"); 
+        var metadata = d3.select("#sample-metadata");
 
-        metadata.forEach((id) => {
-            Object.entries(id).forEach(([key, value]) => {
-                addingData.append("p").text(`${key}: ${value}`); 
-            }); 
-        }); 
+        Object.entries(sample[0]).forEach(([key, value]) => {
+            metadata.append("p").text(`${key}: ${value}`);
+        });
 
-        //console.log("next again"); 
-        //console.log(panelBody); 
-    });  
+        console.log("next again");
+        console.log(metadata);
+    });
 }
 
-//testing function 
-buildMetadata(test); 
+//buildMetadata("940");
 
-//Metadata: 
-//id
-//ethnicity
-//gender
-//age
-//location
-//bbtype
-//wfreq
 
 // Define a function that will create charts for given sample
-function buildCharts(sample) {
+function buildCharts(selection) {
 
     // Read the json data
 
-        // Parse and filter the data to get the sample's OTU data
-        // Pay attention to what data is required for each chart
+    // Parse and filter the data to get the sample's OTU data
+    // Pay attention to what data is required for each chart
 
-        // Create bar chart in correct location
+    // Create bar chart in correct location
 
-        // Create bubble chart in correct location
-    
+    // Create bubble chart in correct location
+
 }
 
 // Define function that will run on page load
 function init() {
 
     // Read json data
+    d3.json("../samples.json").then((sampleData) => {
 
         // Parse and filter data to get sample names
+        var parsedData = sampleData.names;
+        console.log(parsedData);
 
         // Add dropdown option for each sample
+        var dropdownMenu = d3.select("#selDataset");
 
-    // Use first sample to build metadata and initial plots
+        parsedData.forEach((name) => {
+            dropdownMenu.append("option").property("value", name).text(name);
+        })
 
+        // Use first sample to build metadata and initial plots
+
+    });
 }
 
-function optionChanged(newSample){
+function optionChanged(newSelection) {
 
     // Update metadata with newly selected sample
 
