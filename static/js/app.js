30f7e75c36813fbb27aa2680c0762503b5870870
Chloe Veras <chloe@chloes-air.lan>
2020-12-22 16:39:00 -0500

    retrieved proper data for charts in buildCharts function

--- static/js/app.js
+++ static/js/app.js
@@ -6,10 +6,9 @@ function buildMetadata(selection) {
 
         console.log(sampleData);
 
-        console.log("showing parsed data:");
-
         // Parse and filter the data to get the sample's metadata
         var parsedData = sampleData.metadata;
+        console.log("parsed data inside buildMetadata function")
         console.log(parsedData);
 
         var sample = parsedData.filter(item => item.id == selection);
@@ -28,21 +27,43 @@ function buildMetadata(selection) {
     });
 }
 
-//buildMetadata("940");
-
-
 // Define a function that will create charts for given sample
 function buildCharts(selection) {
 
     // Read the json data
+    d3.json("../samples.json").then((sampleData) => {
+    
+        // Parse and filter the data to get the sample's OTU data
+        // Pay attention to what data is required for each chart
+        var parsedData = sampleData.samples; 
+        console.log("parsed data inside buildCharts function")
+        console.log(parsedData); 
 
-    // Parse and filter the data to get the sample's OTU data
-    // Pay attention to what data is required for each chart
+        var barChartValues = parsedData[0].sample_values.slice(0, 10);
+        console.log("sample_values")
+        console.log(barChartValues);
 
-    // Create bar chart in correct location
+        var barChartLabels = parsedData[0].otu_ids.slice(0, 10); 
+        console.log("otu_ids");
+        console.log(barChartLabels);
 
-    // Create bubble chart in correct location
+        var reformattedLabels = [];
+        barChartLabels.forEach((label) => {
+            reformattedLabels.push("OTU " + label); 
+        });
 
+        console.log("reformatted");
+        console.log(reformattedLabels); 
+
+        var barCharthovertext = parsedData[0].otu_labels.slice(0, 10); 
+        console.log("otu_labels");
+        console.log(barCharthovertext); 
+
+        // Create bar chart in correct location
+
+        // Create bubble chart in correct location
+
+    });
 }
 
 // Define function that will run on page load
@@ -53,6 +74,7 @@ function init() {
 
         // Parse and filter data to get sample names
         var parsedData = sampleData.names;
+        console.log("parsed data inside init function")
         console.log(parsedData);
 
         // Add dropdown option for each sample
@@ -63,6 +85,9 @@ function init() {
         })
 
         // Use first sample to build metadata and initial plots
+        buildMetadata(parsedData[0]);
+
+        buildCharts(parsedData[0]);
 
     });
 }
